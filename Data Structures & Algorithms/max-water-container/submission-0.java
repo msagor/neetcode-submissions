577ea4e0f76a5d28e011134f843dd68b9e489def
Mohammad Sagor <32553111+msagor@users.noreply.github.com>
2026-06-08 15:21:01 -0500

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int i=0;
+        int j = heights.length-1;
+
+        int result = 0;
+
+        while(i<j){
+            int width = j-i;
+            int bar1 = heights[i];
+            int bar2 = heights[j];
+
+            int smaller_bar = Math.min(bar1, bar2);
+
+            int area_res = width * smaller_bar;
+
+            if(result<area_res){
+                result = area_res; //new maximum
+
+            }
+
+            //need to move one bar,
+            //it could be bar i or bar j.
+            //we need to move the smaller bar.
+            if(bar1<bar2){
+                i++;
+            }else{
+                j--;
+            }
+
+        }
+
+        return result;
+    }
+}
