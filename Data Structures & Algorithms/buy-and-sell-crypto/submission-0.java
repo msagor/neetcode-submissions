704b13bdfb45f8277e047d49a88ca686bf4772e3
Mohammad Sagor <32553111+msagor@users.noreply.github.com>
2026-06-09 11:46:28 -0500

    Update submission-0.java

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -1,9 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
+        //assume the profit is the lowest possible
         int profit = Integer.MIN_VALUE;
 
+        //assume the buy value is the highest possible
         int minBuyValue = Integer.MAX_VALUE;
+
+        //now we converted this into an optimization problem
+        //we need to decrease the buy value and increase the profit
 
         for(int i=0; i< prices.length; i++){
             int todays_price = prices[i];
@@ -12,12 +17,14 @@ class Solution {
                 minBuyValue = todays_price;
             }else{
                 //not the first time in the market
-                //we can sell only if profit will be higher than what it is now
+                //we can sell only if todays price is higher than minBuyValue
+                //and, profit will be higher than what it is now.
                 if((todays_price>minBuyValue) && (todays_price-minBuyValue>profit)){
                     //good time to sell
                     profit = todays_price-minBuyValue;
-                    System.out.println("current profit is: " + profit);
                 }else{
+                    //selling wont make more profit, so thats out of the question.
+                    //now, need to check if its a good day to buy.
                     //need to update buy value only if todays buy price will be cheaper
                     minBuyValue = Math.min(minBuyValue, todays_price);
                 }
@@ -25,6 +32,10 @@ class Solution {
             }
         }
 
+        //there could be case where we make no profit
+        //for example, if the array is i descending order arr = [10, 7, 5, 3, 2],
+        //in such case we will never make profit
+        //profit can never be negative.
         return (profit==Integer.MIN_VALUE?0:profit);
         
     }
