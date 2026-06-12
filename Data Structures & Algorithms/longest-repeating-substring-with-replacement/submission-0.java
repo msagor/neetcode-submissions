4a154ebf4e1ff6a16ca84b1bb684120dfa04a489
Mohammad Sagor <32553111+msagor@users.noreply.github.com>
2026-06-12 15:52:50 -0500

    Update submission-0.java

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
@@ -1,22 +1,6 @@
 public class Solution {
-    public int characterReplacement(String s, int k) {
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            HashMap<Character, Integer> count = new HashMap<>();
-            int maxf = 0;
-            for (int j = i; j < s.length(); j++) {
-                count.put(s.charAt(j), count.getOrDefault(s.charAt(j), 0) + 1);
-                maxf = Math.max(maxf, count.get(s.charAt(j)));
-                if ((j - i + 1) - maxf <= k) {
-                    res = Math.max(res, j - i + 1);
-                }
-            }
-        }
-        return res;
-    }
-}
-
-/*class Solution {
+    //wrong implementation, does not work
+    //i understood the problem wrong
     public int characterReplacement(String s, int k) {
 
         //create a class tuple
@@ -93,4 +77,4 @@ public class Solution {
 
         return 0;
     }
-}*/
+}
