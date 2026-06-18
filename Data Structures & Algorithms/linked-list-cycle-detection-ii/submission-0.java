41f6d0355db103d6ede0dfce0052d7adb16654e2
Mohammad Sagor <32553111+msagor@users.noreply.github.com>
2026-06-18 18:21:33 -0500

    Create submission-0.java

---
+++ Data Structures & Algorithms/linked-list-cycle-detection-ii/submission-0.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        if(head==null || head.next==null){
+            return null;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+       //tried, had tov use ai to fix
+       // missed fast.next != null check
+        boolean loop_exists = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            //here we are comparing two objects
+            if (fast == slow) {
+                loop_exists = true;
+                break;
+            }
+        }
+
+        if(loop_exists){
+            //get another slow pointer from the beginning
+            ListNode slow1 = head;
+        
+            while(slow1!=slow){
+                slow = slow.next;
+                slow1 = slow1.next;
+            }
+
+            return slow1;
+        }else{
+            return null;
+        }
+    }
+}
