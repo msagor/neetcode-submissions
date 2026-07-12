9200da173a6c303497ecce2febf13eaf425a4f69
Mohammad Sagor <32553111+msagor@users.noreply.github.com>
2026-07-12 02:50:00 -0500

    Update submission-12.java

--- Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-12.java
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-12.java
@@ -9,6 +9,7 @@ class Solution {
         //also notice, for a regular sorted array, nums[left] < nums[mid] < nums[right] holds,
         //but for a rotated sorted array, this equation is disrupted somewhere in the array.
         //for our example: if 6 is mid, 5 is smaller than 6, so the left sub-array [4,5,6] is good.
+        //but 6 is not smaller than 1, so the nums[mid] < nums[right] condition fails.
         //then we immediately know that the disruption is in the right sub-array.
         return binarySearch(nums, 0, nums.length-1);
     }
