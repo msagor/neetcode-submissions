60d0a7ee75e6f24202ebc67eeda8721c61ce1dd8
Mohammad Sagor <32553111+msagor@users.noreply.github.com>
2026-07-13 21:38:58 -0500

    Add: valid-palindrome-ii - submission-8

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-8.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        //valid palindrome with at most one deletion
+        //watched the video then implemented myself
+
+        if(isPalindrome(s)){
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left<right){
+        
+            if(s.charAt(left)!=s.charAt(right)){
+                
+                //get the left string
+                String subS_left = s.substring(left, right);
+
+                //get the right string
+                String subS_right = s.substring(left+1, right+1);
+
+                //since we have at most one deletion available,
+                //simply checking is sufficient
+                if(!isPalindrome(subS_left) && !isPalindrome(subS_right)){
+                    return false;
+                }
+            }
+
+            //when palindrome is good, we come here
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome(String s){
+        String s_rev = new StringBuilder(s).reverse().toString();
+
+        if(s.equals(s_rev)){
+            return true;
+        }
+
+        return false;
+    }
+}
