07ce9c9db02d52db7542b885252009755d2da5fb
Mohammad Sagor <32553111+msagor@users.noreply.github.com>
2026-07-19 00:52:19 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    public int maxProfit(int[] prices) {
+
+        //assume the profit is the lowest possible
+        int profit = Integer.MIN_VALUE;
+
+        //assume the buy value is the highest possible
+        int minBuyValue = Integer.MAX_VALUE;
+
+        //now we converted this into an optimization problem
+        //we need to decrease the buy value and increase the profit
+
+        for(int i=0; i< prices.length; i++){
+            int todays_price = prices[i];
+            if(i==0){
+                //first time in the market so this could be a possible buy value
+                minBuyValue = todays_price;
+            }else{
+                //not the first time in the market
+                //we can sell only if todays price is higher than minBuyValue
+                //and, profit will be higher than what it is now.
+                if((todays_price>minBuyValue) && (todays_price-minBuyValue>profit)){
+                    //good time to sell
+                    profit = todays_price-minBuyValue;
+                }else{
+                    //selling wont make more profit, so thats out of the question.
+                    //now, need to check if its a good day to buy.
+                    //need to update buy value only if todays buy price will be cheaper
+                    minBuyValue = Math.min(minBuyValue, todays_price);
+                }
+
+            }
+        }
+
+        //there could be case where we make no profit
+        //for example, if the array is i descending order arr = [10, 7, 5, 3, 2],
+        //in such case we will never make profit
+        //profit can never be negative.
+        return (profit==Integer.MIN_VALUE?0:profit);
+        
+    }
+
+    //chatgpt solution
+    public int maxProfit_(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            minPrice = Math.min(minPrice, price);
+
+            int profit = price - minPrice;
+
+            maxProfit = Math.max(maxProfit, profit);
+        }
+
+        return maxProfit;
+    }
+}
